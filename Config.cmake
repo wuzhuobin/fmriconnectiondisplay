281626de06ab4c7360c626ad74efc5b55c06b5ab
gundam <gundam@lenovo>
2019-10-12 10:40:12 +0800

    hello world

---
+++ Config.cmake
@@ -0,0 +1,4 @@
+set(
+  CMAKE_PREFIX_PATH
+  D:/ccode/VTK-8.1.1_build
+)
--- src/main.cxx
+++ src/main.cxx
@@ -1,18 +1,7 @@
-#include <QApplication>
-#include <QSurfaceFormat>
-
-#include "MainWindow.h"
- 
+#include <iostream>
 int main( int argc, char** argv )
 {
-  // needed to ensure appropriate OpenGL context is created for VTK rendering.
-  //QSurfaceFormat::setDefaultFormat(QVTKOpenGLNativeWidget::defaultFormat());
-
-  // QT Stuff
-  QApplication app( argc, argv );
- 
-  MainWindow w;
-  w.show();
- 
-  return app.exec();
+  std::cout << "Hello world\n";
+  std::cin.get();
+  return 0;
 }
